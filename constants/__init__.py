705648f4f558af230ba89866d8e1c5fd538a74c4
Ahmed Tammaa <ahmed.tammaa.ai@icloud.com>
2025-03-31 02:36:53 +0200

    [init] easier import for constants

---
+++ constants/__init__.py
@@ -0,0 +1,16 @@
+from .physics import *
+from .simulation import *
+
+__all__ = [
+    "g",
+    "mu_friction",
+    "M_min",
+    "M_total",
+    "K_mass",
+    "L_min",
+    "L_max",
+    "K_spring",
+    "dt",
+    "num_brain_steps",
+    "num_physics_per_brain_step",
+]
